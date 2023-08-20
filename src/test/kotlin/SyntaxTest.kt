b05e7ef0dc981c557f6639e123dd33aa2fae501f
geneaky <kaktus418@gmail.com>
2023-08-21 07:46:10 +0900

    basic syntax 1

--- src/test/kotlin/SyntaxTest.kt
+++ src/test/kotlin/SyntaxTest.kt
@@ -60,4 +60,71 @@ class SyntaxTest {
 
     }
 
+
+    @Test
+    fun `흐름제어`() {
+        val job = "developer"
+
+        //물리적 동치성 비교가 이렇게 되네?
+        if (job == "developer") {
+            println("직업 인증 완료")
+        }else{
+            println("누구세요?")
+        }
+
+        //if else 표현식으로 값을 할당 가능
+        var age : Int = 10
+
+//        val str = if(age > 10) {
+//            "성인"
+//        }else{
+//            "어린이"
+//        }
+        val str = if(age > 10) "성인" else "어린이"
+
+
+        assertEquals(str, "어린이")
+    }
+
+    @Test
+    fun `흐름제어 2`() {
+        val day = 4
+
+        val result : Any = when(day) {
+            1 -> "a"
+            2 -> "b"
+            3 -> "c"
+            else -> 1 //반환타입이 여러개가 되는경우 Any로 지정되나봄
+        }
+
+        assertEquals(result, 1)
+
+        when(getColor()) {
+            "1" -> println("1")
+            "2" -> println("2")
+            //else없이 가능
+        }
+    }
+
+    //컴파일 타임에 함수 반환 타입 추론 가능
+    private fun getColor() = "hi"
+
+    @Test
+    fun `동적으로 반환값이 바뀌는경우 when 식 결과는?`() {
+        when(getNumber(4)) {
+            1 -> println("1")
+            "a" -> println("a")
+        }
+    }
+
+    private fun getNumber(day: Any) =
+        when(day) {
+            1 -> "a"
+            2 -> "b"
+            3 -> "c"
+            else -> 1
+        }
+
+
+
 }
